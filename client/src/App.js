ee58c80d82fb353d5acde8baaca0ff2f803fc718
carldamey <carldamey@gmail.com>
2024-04-25 18:50:40 -0700

    begins implementation of tailwind.css

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,8 @@ import "./App.css"
 
 function App() {
 	return (
-		<div className="App">
-			<h1>CARL DAMEY</h1>
+		<div className="App flex h-screen items-center justify-center">
+			<h1 className="text-5xl">CARL DAMEY</h1>
 		</div>
 	)
 }
